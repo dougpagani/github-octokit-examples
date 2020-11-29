a4d328ce6c5f9a057ca7dd08d2b23df208f00247
dougpagani <redstonefreedom@gmail.com>
2020-11-29 14:33:49 -0500

    Add working hello world call

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const { Octokit } = require("@octokit/core");
 const { helloWorld } = require("octokit-plugin-hello-world");
+
+
+const MyOctokit = Octokit.plugin(helloWorld);
+
+const octokit = new MyOctokit();
+octokit.helloWorld();
+// logs "Hello, world!
