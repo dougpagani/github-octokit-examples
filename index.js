9ace3a38aaf75df2b528d1b9c20495f5042ed3e1
dougpagani <redstonefreedom@gmail.com>
2020-11-29 14:58:50 -0500

    Example call + authentication plumbing

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+})
+
+const x = octokit.pulls.get({
+    owner: "octokit",
+    repo: "rest.js",
+    pull_number: 123,
+}).then(console.log)
+console.log(`x (type: ${typeof x}):`, x)
+
